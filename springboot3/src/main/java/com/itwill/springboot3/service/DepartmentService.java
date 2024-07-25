ddb30889432f2bb2eea5c9028f39dd87abbd6da3
woodstock0513 <judydayoung@gmail.com>
2024-07-25 17:25:24 +0900

    0725 부서 목록 페이지

--- springboot3/src/main/java/com/itwill/springboot3/service/DepartmentService.java
+++ springboot3/src/main/java/com/itwill/springboot3/service/DepartmentService.java
@@ -2,7 +2,12 @@ package com.itwill.springboot3.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.itwill.springboot3.domain.Department;
 import com.itwill.springboot3.repository.DepartmentRepository;
@@ -17,9 +22,17 @@ public class DepartmentService {
 
 	private final DepartmentRepository deptRepo;
 	
-	public List<Department> read(){
-		log.info("read()");
-		return deptRepo.findAll();
+	@Transactional(readOnly = true)
+	public Page<Department> read(int pageNo, Sort sort){
+		log.info("read(pageNo={}, sort={})",pageNo,sort);
+		Pageable pageable = PageRequest.of(pageNo, 5, sort);
+		Page<Department> page = deptRepo.findAll(pageable);
+		log.info("hasPrevious = {}",page.hasPrevious()); //이전 페이지가 있는 지 여부
+		log.info("hasNext = {}",page.hasNext()); //다음 페이지가 있는 지 여부
+		log.info("getContent = {}",page.getContent()); //페이지 처리가 끝난 List
+		log.info("getTotalPages = {}",page.getTotalPages()); //전체 페이지 개수
+		
+		return page;
 	}
 	
 	public Department readById(int id) {
--- springboot3/src/main/java/com/itwill/springboot3/web/DepartmentController.java
+++ springboot3/src/main/java/com/itwill/springboot3/web/DepartmentController.java
@@ -3,11 +3,14 @@ package com.itwill.springboot3.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.itwill.springboot3.domain.Department;
 import com.itwill.springboot3.repository.DepartmentRepository;
@@ -24,10 +27,10 @@ public class DepartmentController {
 	private final DepartmentService deptSvc;
 	
 	@GetMapping("/list")
-	public void departmentList(Model model) {
-		log.info("departmentList()");
-		List<Department> list = deptSvc.read();
-		model.addAttribute("departments", list);
+	public void departmentList(@RequestParam(name = "p", defaultValue = "0") int pageNo, Model model) {
+		log.info("departmentList(pageNo = {})",pageNo);
+		Page<Department> list = deptSvc.read(pageNo, Sort.by("id"));
+		model.addAttribute("page", list);
 	}
 	
 	@GetMapping("details/{id}")
